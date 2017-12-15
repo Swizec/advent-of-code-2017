327e7a27539d8a703b47e049990847e3c773f5e3
Swizec Teller <swizec@swizec.com>
2017-12-14 22:43:44 -0800

    Day 15, star 2

--- 15.js
+++ 15.js
@@ -1,4 +1,4 @@
-function* generator(factor, startingValue) {
+function* star1generator(factor, startingValue, _) {
     let val = startingValue;
     while (true) {
         val = (val * factor) % 2147483647;
@@ -6,14 +6,30 @@ function* generator(factor, startingValue) {
     }
 }
 
-function star1(startA, startB) {
-    const A = generator(16807, startA),
-        B = generator(48271, startB);
+function* star2generator(factor, startingValue, denominator) {
+    let val = startingValue;
+    while (true) {
+        val = (val * factor) % 2147483647;
+        if (val % denominator === 0) {
+            yield val;
+        }
+    }
+}
+
+function countMatches({
+    startA,
+    startB,
+    generator,
+    denominators = [],
+    sampleSize
+}) {
+    const A = generator(16807, startA, denominators[0]),
+        B = generator(48271, startB, denominators[1]);
 
     let judge = 0,
         mask = 0b00000000000000001111111111111111;
 
-    for (let i = 0; i < 40000000; i++) {
+    for (let i = 0; i < sampleSize; i++) {
         if ((A.next().value & mask) === (B.next().value & mask)) {
             judge += 1;
         }
@@ -22,4 +38,14 @@ function star1(startA, startB) {
     return judge;
 }
 
-console.log(star1(591, 393));
+// console.log(countMatches({ startA: 591, startB: 393, generator: star1generator, sampleSize: 40000000 }));
+
+console.log(
+    countMatches({
+        startA: 591,
+        startB: 393,
+        generator: star2generator,
+        denominators: [4, 8],
+        sampleSize: 5000000
+    })
+);
